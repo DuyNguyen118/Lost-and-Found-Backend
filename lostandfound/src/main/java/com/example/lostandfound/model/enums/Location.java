8f8377009a4ea598eba398dc3aecaa0cc072cb84
Van Anh <anhoconnell@gmail.com>
2024-12-30 15:16:28 +0700

    fix: location enum methods

--- lostandfound/src/main/java/com/example/lostandfound/model/enums/Location.java
+++ lostandfound/src/main/java/com/example/lostandfound/model/enums/Location.java
@@ -1,5 +1,26 @@
 package com.example.lostandfound.model.enums;
 
 public enum Location {
+    LIBRARY("Library"),
+    CANTEEN("Canteen"),
+    A1_BUILDING("A1 Building"),
+    A2_BUILDING("A2 Building"),
+    PARKING_AREA("Parking Area"),
+    CENTRAL_LIBRARY("Central Library"),
+    ER_INSTITUTE("ER Institute");
 
+    private final String displayName;
+
+    Location(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
