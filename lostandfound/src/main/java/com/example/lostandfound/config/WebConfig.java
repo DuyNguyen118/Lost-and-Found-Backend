4f9c6003841b49f268d67e39fbfc43cf21b9260b
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-01-03 04:21:33 +0700

    fix: link to frontend url

--- lostandfound/src/main/java/com/example/lostandfound/config/WebConfig.java
+++ lostandfound/src/main/java/com/example/lostandfound/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://localhost:5000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
