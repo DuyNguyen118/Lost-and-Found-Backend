8f500b34ac0c4e60bd890f5513a65543bdcfeb36
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-01-03 16:31:00 +0700

    feat: create request class for register

---
+++ lostandfound/src/main/java/com/example/lostandfound/dto/RegisterRequest.java
@@ -0,0 +1,52 @@
+package com.example.lostandfound.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class RegisterRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Student ID is required")
+    private String studentId;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+}
