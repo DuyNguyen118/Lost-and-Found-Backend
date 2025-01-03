cd8dd0d918e0c7be2027c8ae822c53d1076a3998
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-01-04 05:05:40 +0700

    feat: change api to map with frontend

--- lostandfound/src/main/java/com/example/lostandfound/controller/ItemController.java
+++ lostandfound/src/main/java/com/example/lostandfound/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.example.lostandfound.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,7 +39,7 @@ public class ItemController {
     }
 
     // Get all reported items
-    @GetMapping("/all")
+    @GetMapping("/list")
     public List<Item> getAllItems() {
         return itemService.getAllItems();
     }
