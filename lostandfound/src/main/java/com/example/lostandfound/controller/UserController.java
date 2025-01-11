7fbee4c9d99fb3cb95493cae6e711c87d049e88b
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-01-11 15:09:28 +0700

    fix: use user controller, add library

--- lostandfound/src/main/java/com/example/lostandfound/controller/UserController.java
+++ lostandfound/src/main/java/com/example/lostandfound/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.lostandfound.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.lostandfound.model.User;
 import com.example.lostandfound.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -33,9 +39,9 @@ public class UserController {
     }
 
     // Delete a user
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable int id) {
-        userService.deleteUser(id);
-        return "User deleted successfully.";
-    }
+    // @DeleteMapping("/{id}")
+    // public String deleteUser(@PathVariable int id) {
+    //     userService.deleteUser(id);
+    //     return "User deleted successfully.";
+    // }
 }
